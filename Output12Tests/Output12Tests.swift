57ca53e982080d8e79e168e00decff20f27ca5d2
Victor Idongesit <victoridongesit@gmail.com>
2017-12-01 09:09:46 +0100

    Initial Commit

---
+++ Output12Tests/Output12Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Output12Tests.swift
+//  Output12Tests
+//
+//  Created by Victor Idongesit on 01/12/2017.
+//  Copyright © 2017 Victor Idongesit. All rights reserved.
+//
+
+import XCTest
+@testable import Output12
+
+class Output12Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
